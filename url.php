f97e4dd0dcf2df40301b61204669fdf3f16e4b08
Apeliemma <livingstoneapeli@gmail.com>
2025-04-01 12:39:49 +0300

    updated

--- url.php
+++ url.php
@@ -8,4 +8,6 @@ return [
     "resend_verification" => "$BASE_URL/resend_verification.php",
     "reset_password" => "$BASE_URL/reset_password.php",
     "forgot_password" => "$BASE_URL/forgot_password.php",
+    "login" => "$BASE_URL/login.php",
+    "logout" => "$BASE_URL/logout.php",
 ];
